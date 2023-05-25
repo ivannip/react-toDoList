ab7963e1c17d67f1edbd61a32dbf1d3f828c0bee
Ivan <ihpn1802@gmail.com>
2023-05-25 16:53:24 +0100

    push to github

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Footer() {
   const year = new Date().getFullYear();
-  return (<footer><p>copyright {year}</p></footer>);
+  return (
+    <footer>
+      <p><a href="https://github.com/ivannip/react-toDoList">Git Hub Source Here</a> - copyright {year} </p>
+    </footer>
+    );
 };
 
 export default Footer;
